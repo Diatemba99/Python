00c41a7a44758dac21ee62f5b601ed521351ac13
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2020-06-20 02:33:07 +0000

    Add files via upload
    corrigé du tp1

---
+++ tp1/achat_actions.py
@@ -0,0 +1,17 @@
+"""
+ceci est un script qui permet de faire le calcul d'action
+"""
+
+
+
+
+
+cout_par_action = 25.625
+
+nombre_actions  = 400
+
+montant = nombre_actions * cout_par_action
+
+print("le montant est de : ",montant)
+
+
---
+++ tp1/profit.py
@@ -0,0 +1,12 @@
+"""
+ceci est un script qui permet de faire le calcul de prrofit
+"""
+
+
+
+
+revenu = 98456
+couts  = 45000
+benefice = revenu - couts
+
+print("Le bénéfice est de",benefice)
