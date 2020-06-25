dffad70ed3690441b388db0a1413d3bc3acfc263
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2020-06-25 04:57:15 +0000

    Add files via upload

---
+++ tp2/afff.py
@@ -0,0 +1,13 @@
+
+b= ["gambie", "pata", "mali", "SENEGAL", ["jes", "bro"]]
+for n in b:
+    print(n, end=' ')
+    for m in n:
+        print(m, end=' ')
+
+
+
+c= ["hhh", "jj", "lll", "SENEGAL",["waw", "deff"],["fils", "10000"]]
+
+for i in c:
+    print(''.join(i))
