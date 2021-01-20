3ac3839f3e370f4cbfcfb01624bc311831e96326
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-20 21:55:06 +0000

    Update exo5.py

--- tp2/exo5.py
+++ tp2/exo5.py
@@ -3,11 +3,7 @@ ceci est un programme qui utilise une boucle for pour afficher des entiers de 1 
 compris et utiliser l’instruction break pour interrompre la boucle lorsque la variable
 d’itération est égale à 5.
 """
-
-
-
-for i in range(1,10):
-    
+for i in range(1,10):    
     print(i)
     if i == 5:
         break
