332b98d48a082e182037010e4fd0ded3209a5513
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-20 21:39:05 +0000

    Update achat_actions.py

--- tp1/achat_actions.py
+++ tp1/achat_actions.py
@@ -1,17 +1,12 @@
 """
 ceci est un script qui permet de faire le calcul d'action
 """
-
-
-
-
-
 cout_par_action = 25.625
-
 nombre_actions  = 400
-
 montant = nombre_actions * cout_par_action
 
-print("le montant est de : ",montant)
-
-
+print("--------------------------------")
+print("|                               |")
+print("| le montant est de: ",montant,"  |")
+print("|                               |")
+print("--------------------------------")
