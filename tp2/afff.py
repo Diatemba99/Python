698c94ebd4dc50d4239335db089c1036acf07c9d
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-20 21:44:11 +0000

    Update afff.py

--- tp2/afff.py
+++ tp2/afff.py
@@ -1,13 +1,11 @@
-
-b= ["gambie", "pata", "mali", "SENEGAL", ["jes", "bro"]]
+# author @Diatemba99
+b= ["gambie", "pata", "mali", "SENEGAL", ["yeah", "bro"]]
 for n in b:
     print(n, end=' ')
     for m in n:
         print(m, end=' ')
 
-
-
-c= ["hhh", "jj", "lll", "SENEGAL",["waw", "deff"],["fils", "10000"]]
+c= ["doing", "code", "for", "better",["life", "yeahh"],["fils", "10000"]]
 
 for i in c:
     print(''.join(i))
