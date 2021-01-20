e66aca7ee5ff42074c9a48e8420bcbea94c1e6cd
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-20 21:52:47 +0000

    Update exo4.py

--- tp2/exo4.py
+++ tp2/exo4.py
@@ -1,11 +1,7 @@
 """"
 ceci est script qui permet de creer un sapin a partir des  donnees de l'utilisateur
 """
-
 taille_sapin = int(input("veuillez saisir une valeur "))
-
-
 for i in range(taille_sapin + 1):
-   
-    print("\t", "*" *i)
+       print("\t", "*" *i)
 
