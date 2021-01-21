0a25fd8054f05cd6b1b6d4504774af646e0171e5
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-21 14:33:05 +0000

    Update exo6.py

--- tp3/exo6.py
+++ tp3/exo6.py
@@ -3,8 +3,6 @@ Ecrivez un script qui recopie une chaîne (dans une nouvelle variable), en insé
 des astérisques entre les caractères.
 Ainsi, par exemple, « Python » devra donner « P*y*t*h*o*n »
 """
-
 nom = input("tapez un mot ")
-
 #affichage en ajoutant * apres chaque caractere
 print('*'.join(nom))
