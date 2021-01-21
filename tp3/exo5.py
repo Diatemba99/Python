947abc9abfadbc56d1a3f02511f0798b99a517e6
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-21 18:50:15 +0000

    Update exo5.py

--- tp3/exo5.py
+++ tp3/exo5.py
@@ -2,18 +2,14 @@
  Dans cet exercice, vous allez créer un
 programme qui lit une année puis le nom d'un mois respectivement saisis par
 l'utilisateur. Ensuite, votre programme devrait afficher le nombre de jours de ce mois.
-
 Si le nom du mois saisi est fevrier et si l’année est bissextile alors le nombre de jours
 de fevrier est 29. Si l’année n’est pas bissextile, alors le nombre de jours de fevrier
 est 28. Pour faire plus simple, le nom du mois saisi par l’utilisateur doit être converti
 en minuscule par le programme et ne doit pas comporté d’accent.
 """
 
-
 annee_saisie = int(input("Veuiller saisir une annee "))
-
 mois = input("Veuiller saisir un mois ex:[mai] ")
-
 mois_saisie = mois.lower()
 
 #cette variable me permet de changer le nombre de jours du mois de fevrier si l'annee n'est pas bissextile
@@ -21,12 +17,8 @@ fevrier = 28
 
 char = {'janvier':'31', 'fevrier':'29', 'mars':'31', 'avril':'30', 'mai':'31', 'juin':'30', 'juillet':'31', 'aout':'31', 'septembre':'30', 'octobre':'31', 'novembre':'30', 'decembre':'31' }
 
-
-
-
 #gerer le mois de fevrier
-
-#si l'annee saisie est bissextile exexcute ce code
+#si l'annee saisie est bissextile execute ce code
 if (annee_saisie % 400 == 0) or ((annee_saisie % 4 ==0) & (annee_saisie / 100 !=0)):
     for i in char:
         if mois_saisie == i:
@@ -35,7 +27,7 @@ if (annee_saisie % 400 == 0) or ((annee_saisie % 4 ==0) & (annee_saisie / 100 !=
             print("mois invalide")
             break
 
-#si l'annee saisie n'est pas bissextile exexcute ce code
+#si l'annee saisie n'est pas bissextile execute ce code
 else:
     #je change le nombre de jour de fevrier par 28
     char['fevrier'] = fevrier
@@ -45,18 +37,3 @@ else:
         else:
             print("mois invalide")
             break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
