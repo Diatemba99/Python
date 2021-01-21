7f52f5e3cd58a035d300f51a30b55530573ebd15
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-21 14:25:31 +0000

    Update exo2.py

--- tp3/exo2.py
+++ tp3/exo2.py
@@ -1,22 +1,14 @@
 """"
-Ecrivez un programme qui affiche toutes les lettres d’une chaîne1 présentent dans
+Ecrivez un programme qui affiche toutes les lettres d’une chaîne présentent dans
 une chaîne2
 """
-
 chaine = input("taper le premier mot ")
-
 chaine2 = input("taper le deuxieme mot ")
-
-
 
 #je parcours chaque caractere du premier mot
 for i in chaine:
-
     # je parcours chaque caractere du deuxieme mot
     for j in chaine2:
-
         #j'affiche les caracteres present dans les deux mots
         if i == j:
-
             print(i)
-
