e9924459afbc4e885195e531386f248e1c4145f4
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-22 06:58:35 +0000

    Update exo2.py

--- tp4/exo2.py
+++ tp4/exo2.py
@@ -1,14 +1,11 @@
 """
 Les instructions s = 'a' + 'b' et s = " " .join (['a', 'b']) attribuent-elles la même valeur à la
-variable s ?
+variable s ?
 """
-
 s = 'a' + 'b'
 print(s)
 s1 = " " .join (['a', 'b'])
 print(s1)
-
-
 """
 la reponse est non car l'espace est considere comme caractere
 len(s)=2
