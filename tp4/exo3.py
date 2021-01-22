ca850b94fc288c27ed7c9faa3d1cedcd25847b6e
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-22 06:59:13 +0000

    Update exo3.py

--- tp4/exo3.py
+++ tp4/exo3.py
@@ -2,8 +2,6 @@
 Ecrivez un programme qui affiche chacun de ces prénoms avec le nombre de
 caractères correspondants
 """
-
-
 liste = [ "Abdoul", "Jean-Pierre", "Penda", "Véronique",
           "Martine", "Amadou", "Ibrahima" ]
 
