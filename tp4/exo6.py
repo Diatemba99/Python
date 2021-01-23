513a7cbfc7be34031ef6bbe282a2a3171d84a567
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-23 22:57:07 +0000

    Update exo6.py

--- tp4/exo6.py
+++ tp4/exo6.py
@@ -6,9 +6,6 @@ c. Afficher la liste des valeurs du dictionnaire.
 d. Afficher la liste des paires clé/valeur du dictionnaire.
 e. Ecrire la phrase "Jacques Dupuis a 30 ans". 
 """
-
-
-
 d = {'nom': 'Dupuis', 'prenom': 'Jacque', 'age': 30}
 
 #changer jacque par jacques
@@ -26,21 +23,11 @@ print("affichage des valeurs")
 for j in d:
     print(" valeur : {}".format(d[j]))
 
-
 print("\n\n")
 print("affichage des clefs et valeurs")
 for l in d:
     print("clef: {} : {}".format(l,d[l]))
 
-
-
 print("\n\n")
 print("Ecrire la phrase Jacques Dupuis a 30 ans \t")
 print(d['prenom'], d['nom'], "a", d['age'],"ans")
-
-
-
-
-
-
-
