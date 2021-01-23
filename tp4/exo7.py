ce707c2debc35766b661bb9e380ad6bf6904ecb5
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-23 22:58:35 +0000

    Update exo7.py

--- tp4/exo7.py
+++ tp4/exo7.py
@@ -1,17 +1,13 @@
 """
 tranformation en dictionnaire
-
 """
-
-
 dico = {
         'employe1': '["Ibrahima", "Fall", "Masculin", 35, "Enseignant", "Castors"]',
         'employe2': '["Fanta", "Kaba", "Féminin",  25, "Etudiante",  "HLM"]',
         'employe3': '["Jean-Pierre", "Ngom", "Masculin", 45, "Informaticien", "Grand Yoff"]',
         'employe4': '["Aziz", "Mbaye", "Masculin", 30, "Commercant", "Colobane"]',
         'employe5': '["Hawa", "Niang", "Féminin", 35, "Comptable", "Derkle"]'
-
-        }
+      }
 
 print(dico['employe1'])
 print(dico['employe2'])
