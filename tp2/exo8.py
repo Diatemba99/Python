04462c437ea657310f7fa9bf3fd99d6a9bc7ff2d
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-02-16 23:57:31 +0000

    Update exo8.py

--- tp2/exo8.py
+++ tp2/exo8.py
@@ -1,7 +1,9 @@
 """programme table de multiplication
 """
 nombre = int(input("entre le nombre de la table de multiplication "))
+
 for i in range(1,nombre+1):
+    
     print(end=' ')
     for j in range(1,10):
            print("\t",j, "x", i, "=", i*j, "\n")
