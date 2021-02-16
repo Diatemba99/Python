abb846944c6385e5aa5f4d94087799945eab5b95
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-02-16 23:56:46 +0000

    Update achat_actions.py

--- tp1/achat_actions.py
+++ tp1/achat_actions.py
@@ -4,7 +4,6 @@ ceci est un script qui permet de faire le calcul d'action
 cout_par_action = 25.625
 nombre_actions  = 400
 montant = nombre_actions * cout_par_action
-
 print("--------------------------------")
 print("|                               |")
 print("| le montant est de: ",montant,"  |")
