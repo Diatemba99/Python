79a964129508e4619746a353cb0d5665784aad91
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-02-16 23:57:06 +0000

    Update exo1.py

--- tp4/exo1.py
+++ tp4/exo1.py
@@ -5,7 +5,6 @@ societes = [("Sonatel", "Mermoz", "Dakar"), ("CSS", "Richars Tall", Saint Louis"
 (nom, ville, region) = (societes [1][0], societes [1][1], societes [1][2])
 print(nom, " est située à ", ville, ", ", region, '.',sep = " ")
 """
-
 societes = [("Sonatel", "Mermoz", "Dakar"),
             ("CSS", "Richars Tall", "Saint Louis"),
             ("EDK", "Escale", "Kolda")
